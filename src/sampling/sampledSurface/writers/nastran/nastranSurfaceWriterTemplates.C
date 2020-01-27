89e7775f908c591cee0cf5b93ea5ce8ffe8c00b9
Mark Olesen <Mark.Olesen@esi-group.com>
2020-01-27 11:04:25 +0100

    BUG: incorrect Nastran surface output and segmentation faults #1571
    - indexing error in the output of values resulted in uniform output in
      most cases.
    
    - allocation error for on-the-fly triangulation
    
    ENH: changed decomposed storage from DynamicList to plain faceList for
    clearer allocation control and better overhead

--- src/sampling/sampledSurface/writers/nastran/nastranSurfaceWriterTemplates.C
+++ src/sampling/sampledSurface/writers/nastran/nastranSurfaceWriterTemplates.C
@@ -187,7 +187,7 @@ Foam::fileName Foam::nastranSurfaceWriter::writeTemplate
         << "$" << nl
         << "BEGIN BULK" << nl;
 
-    List<DynamicList<face>> decomposedFaces;
+    List<faceList> decomposedFaces;
     writeGeometry(os, surf, decomposedFaces);
 
     os  << "$" << nl
@@ -198,7 +198,7 @@ Foam::fileName Foam::nastranSurfaceWriter::writeTemplate
 
     if (isNodeValues)
     {
-        for (const DynamicList<face>& dFaces : decomposedFaces)
+        for (const faceList& dFaces : decomposedFaces)
         {
             for (const face& f : dFaces)
             {
@@ -216,12 +216,16 @@ Foam::fileName Foam::nastranSurfaceWriter::writeTemplate
     }
     else
     {
-        for (const DynamicList<face>& dFaces : decomposedFaces)
+        auto valIter = values.cbegin();
+
+        for (const faceList& dFaces : decomposedFaces)
         {
             forAll(dFaces, facei)
             {
-                writeFaceValue(os, format, values[facei], ++elemId);
+                writeFaceValue(os, format, *valIter, ++elemId);
             }
+
+            ++valIter;
         }
     }
 
