169e482066a4682d81bf712e4e13a77b71c74fd6
Mark Olesen <Mark.Olesen@esi-group.com>
2020-03-16 14:35:30 +0100

    BUG: missing output for foamDictionary -includes (closes #1635)
    - log to stdout when explicitly enabled

--- src/OpenFOAM/db/dictionary/functionEntries/includeEtcEntry/includeEtcEntry.C
+++ src/OpenFOAM/db/dictionary/functionEntries/includeEtcEntry/includeEtcEntry.C
@@ -6,6 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2015-2017 OpenFOAM Foundation
+    Copyright (C) 2019-2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -104,7 +105,8 @@ bool Foam::functionEntries::includeEtcEntry::execute
     {
         if (Foam::functionEntries::includeEtcEntry::log)
         {
-            DetailInfo << fName << endl;
+            // Report to stdout which file is included
+            Info<< fName << nl;
         }
         parentDict.read(ifs);
         return true;
@@ -137,7 +139,8 @@ bool Foam::functionEntries::includeEtcEntry::execute
     {
         if (Foam::functionEntries::includeEtcEntry::log)
         {
-            DetailInfo << fName << endl;
+            // Report to stdout which file is included
+            Info<< fName << nl;
         }
         entry.read(parentDict, ifs);
         return true;
