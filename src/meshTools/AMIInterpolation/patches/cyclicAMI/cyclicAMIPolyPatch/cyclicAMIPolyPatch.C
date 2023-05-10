e537d5c68d5ca43dae067a9d79836bc0607a6c82
Andrew Heather <>
2023-05-10 21:29:07 +0100

    ENH: AMI - added caching of weights and addressing
    Applicable to rotational cases:
    
    - stores AMI weights and addressing on the first revolution
    - cached evaluations performed on subsequent revolutions to reduce computational
      costs
    
    Cached values are stored in angular bins, specified using the [optional]
    `cacheSize` entry when defining the patch in the polyMesh/boundary file, e.g.
    
        AMI1
        {
            type            cyclicAMI;
            AMIMethod       faceAreaWeightAMI;
            neighbourPatch  AMI2;
    
            cacheSize       360; // New entry
            transform       rotational;
            rotationAxis    (0 0 1);
            rotationCentre  (0 0 0);
        }
    
    Note that the transform must also be set to rotational; the additional
    `rotationAxis` and `rotationCentre` entries are used to construct a local AMI
    co-ordinate system to determine the rotation angle as the mesh moves.
    
    360 bins are created in the example above, equating to a uniform bin width
    of 1 degree.

--- src/meshTools/AMIInterpolation/patches/cyclicAMI/cyclicAMIPolyPatch/cyclicAMIPolyPatch.C
+++ src/meshTools/AMIInterpolation/patches/cyclicAMI/cyclicAMIPolyPatch/cyclicAMIPolyPatch.C
@@ -381,6 +381,45 @@ void Foam::cyclicAMIPolyPatch::resetAMI(const UList<point>& points) const
         return;
     }
 
+    point refPt = point::max;
+
+    bool restoredFromCache = false;
+    if (AMIPtr_->cacheActive())
+    {
+        const label comm = boundaryMesh().mesh().comm();
+
+        if (UPstream::parRun())
+        {
+            label refProci = -1;
+            if (size() > 0)
+            {
+                refProci = UPstream::myProcNo();
+            }
+            reduce(refProci, maxOp<label>(), UPstream::msgType(), comm);
+
+            if (refProci == UPstream::myProcNo())
+            {
+                refPt = points[meshPoints()[0]];
+            }
+            reduce(refPt, minOp<point>(), UPstream::msgType(), comm);
+        }
+        else
+        {
+            refPt = points[meshPoints()[0]];
+        }
+
+
+        // Sets cache indices to use and time interpolation weight
+        restoredFromCache = AMIPtr_->restoreCache(refPt);
+
+        if (returnReduceOr(restoredFromCache, comm))
+        {
+            // Restored AMI weight and addressing from cache - all done
+            return;
+        }
+    }
+
+
     const cyclicAMIPolyPatch& nbr = neighbPatch();
     const pointField srcPoints(points, meshPoints());
     pointField nbrPoints(points, nbr.meshPoints());
@@ -432,7 +471,7 @@ void Foam::cyclicAMIPolyPatch::resetAMI(const UList<point>& points) const
         meshTools::writeOBJ(osN, nbrPatch0.localFaces(), nbrPoints);
 
         OFstream osO(t.path()/name() + "_ownerPatch.obj");
-        meshTools::writeOBJ(osO, this->localFaces(), localPoints());
+        meshTools::writeOBJ(osO, this->localFaces(), srcPoints);
     }
 
     // Construct/apply AMI interpolation to determine addressing and weights
@@ -446,6 +485,8 @@ void Foam::cyclicAMIPolyPatch::resetAMI(const UList<point>& points) const
     {
         AMIPtr_->checkSymmetricWeights(true);
     }
+
+    AMIPtr_->addToCache(refPt, rotationAxis_, rotationCentre_);
 }
 
 
@@ -751,6 +792,13 @@ Foam::cyclicAMIPolyPatch::cyclicAMIPolyPatch
         srcFaceIDs_.setSize(dict.get<label>("srcSize"));
         tgtFaceIDs_.setSize(dict.get<label>("tgtSize"));
         moveFaceCentres_ = dict.getOrDefault("moveFaceCentres", true);
+    }
+
+    if (AMIPtr_->cacheActive() && transform() != ROTATIONAL)
+    {
+        FatalErrorInFunction
+            << "AMI Caching is only available for rotational transforms"
+            << exit(FatalError);
     }
 }
 
