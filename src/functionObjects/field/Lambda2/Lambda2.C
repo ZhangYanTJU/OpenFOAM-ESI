a252677618928dff3b447478315255514f2aa02b
Kutalmis Bercin <kutalmis.bercin@esi-group.com>
2023-09-25 17:14:11 +0100

    BUG: Lambda2: reverse the sign of the governing equation (fixes #2731)

--- src/functionObjects/field/Lambda2/Lambda2.C
+++ src/functionObjects/field/Lambda2/Lambda2.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2013-2016 OpenFOAM Foundation
-    Copyright (C) 2019-2020 OpenCFD Ltd.
+    Copyright (C) 2019-2023 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -61,10 +61,11 @@ bool Foam::functionObjects::Lambda2::calc()
             )
         );
 
+        // (JH:p. 76-78)
         return store
         (
             resultName_,
-           -eigenValues(SSplusWW)().component(vector::Y)
+            eigenValues(SSplusWW)().component(vector::Y)
         );
     }
 
