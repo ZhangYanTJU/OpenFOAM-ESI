ZhangYanTJU/OpenFOAM-ESI

commit 8fb148bb0e9b39955cefd239753b3547d2e6ee0a

ENH: lagrangian cloud function objects - added track data to hooks

src/lagrangian/intermediate/submodels/CloudFunctionObjects/CloudFunctionObjectList/CloudFunctionObjectList.C
@@ -110,9 +110,9 @@ void Foam::CloudFunctionObjectList<CloudType>::preEvolve
     const typename parcelType::trackingData& td
 )
 {
-    forAll(*this, i)
+    for (auto& cfo : *this)
     {
-        this->operator[](i).preEvolve(td);
+        cfo.preEvolve(td);
     }
 }
 
@@ -123,70 +123,71 @@ void Foam::CloudFunctionObjectList<CloudType>::postEvolve
     const typename parcelType::trackingData& td
 )
 {
-    forAll(*this, i)
+    for (auto& cfo : *this)
     {
-        this->operator[](i).postEvolve(td);
+        cfo.postEvolve(td);
     }
 }
 
 
 template<class CloudType>
-void Foam::CloudFunctionObjectList<CloudType>::postMove
+bool Foam::CloudFunctionObjectList<CloudType>::postMove
 (
     parcelType& p,
     const scalar dt,
     const point& position0,
-    bool& keepParticle
+    const typename parcelType::trackingData& td
 )
 {
-    forAll(*this, i)
+    for (auto& cfo : *this)
     {
-        if (!keepParticle)
+        if (!cfo.postMove(p, dt, position0, td))
         {
-            return;
+            return false;
         }
-
-        this->operator[](i).postMove(p, dt, position0, keepParticle);
     }
+
+    return true;
 }
 
 
 template<class CloudType>
-void Foam::CloudFunctionObjectList<CloudType>::postPatch
+bool Foam::CloudFunctionObjectList<CloudType>::postPatch
 (
-    const parcelType& p,
+    parcelType& p,
     const polyPatch& pp,
-    bool& keepParticle
+    const typename parcelType::trackingData& td
 )
 {
-    forAll(*this, i)
+    for (auto& cfo : *this)
     {
-        if (!keepParticle)
+        if (!cfo.postPatch(p, pp, td))
         {
-            return;
+            return false;
         }
-
-        this->operator[](i).postPatch(p, pp, keepParticle);
     }
+
+    return true;
 }
 
 
 template<class CloudType>
-void Foam::CloudFunctionObjectList<CloudType>::postFace
+bool Foam::CloudFunctionObjectList<CloudType>::postFace
 (
-    const parcelType& p,
-    bool& keepParticle
+    parcelType& p,
+    const typename parcelType::trackingData& td
 )
 {
-    forAll(*this, i)
+    for (auto& cfo : *this)
     {
-        if (!keepParticle)
+        if (!cfo.postFace(p, td))
         {
-            return;
+            return false;
         }
 
-        this->operator[](i).postFace(p, keepParticle);
     }
+
+    return true;
 }
 
 

src/lagrangian/intermediate/submodels/CloudFunctionObjects/CloudFunctionObject/CloudFunctionObject.C
@@ -109,33 +109,39 @@ void Foam::CloudFunctionObject<CloudType>::postEvolve
 
 
 template<class CloudType>
-void Foam::CloudFunctionObject<CloudType>::postMove
+bool Foam::CloudFunctionObject<CloudType>::postMove
 (
     parcelType&,
     const scalar,
     const point&,
-    bool&
+    const typename parcelType::trackingData& td
 )
-{}
+{
+    return true;
+}
 
 
 template<class CloudType>
-void Foam::CloudFunctionObject<CloudType>::postPatch
+bool Foam::CloudFunctionObject<CloudType>::postPatch
 (
     const parcelType&,
     const polyPatch&,
-    bool&
+    const typename parcelType::trackingData& td
 )
-{}
+{
+    return true;
+}
 
 
 template<class CloudType>
-void Foam::CloudFunctionObject<CloudType>::postFace
+bool Foam::CloudFunctionObject<CloudType>::postFace
 (
     const parcelType&,
-    bool&
+    const typename parcelType::trackingData& td
 )
-{}
+{
+    return true;
+}
 
 
 template<class CloudType>

src/lagrangian/intermediate/submodels/CloudFunctionObjects/CloudFunctionObject/CloudFunctionObject.H
@@ -159,27 +159,27 @@ public:
             );
 
             //- Post-move hook
-            virtual void postMove
+            virtual bool postMove
             (
                 parcelType& p,
                 const scalar dt,
                 const point& position0,
-                bool& keepParticle
+                const typename parcelType::trackingData& td
             );
 
             //- Post-patch hook
-            virtual void postPatch
+            virtual bool postPatch
             (
                 const parcelType& p,
                 const polyPatch& pp,
-                bool& keepParticle
+                const typename parcelType::trackingData& td
             );
 
             //- Post-face hook
-            virtual void postFace
+            virtual bool postFace
             (
                 const parcelType& p,
-                bool& keepParticle
+                const typename parcelType::trackingData& td
             );
 
 

src/lagrangian/intermediate/submodels/CloudFunctionObjects/CloudFunctionObjectList/CloudFunctionObjectList.H
@@ -125,27 +125,27 @@ public:
             );
 
             //- Post-move hook
-            virtual void postMove
+            virtual bool postMove
             (
                 parcelType& p,
                 const scalar dt,
                 const point& position0,
-                bool& keepParticle
+                const typename parcelType::trackingData& td
             );
 
             //- Post-patch hook
-            virtual void postPatch
+            virtual bool postPatch
             (
-                const parcelType& p,
+                parcelType& p,
                 const polyPatch& pp,
-                bool& keepParticle
+                const typename parcelType::trackingData& td
             );
 
             //- Post-face hook
-            virtual void postFace
+            virtual bool postFace
             (
-                const parcelType& p,
-                bool& keepParticle
+                parcelType& p,
+                const typename parcelType::trackingData& td
             );
 };
 

src/lagrangian/intermediate/submodels/CloudFunctionObjects/FaceInteraction/FaceInteraction.C
@@ -263,12 +263,14 @@ Foam::FaceInteraction<CloudType>::FaceInteraction
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class CloudType>
-void Foam::FaceInteraction<CloudType>::postFace
+bool Foam::FaceInteraction<CloudType>::postFace
 (
     const parcelType& p,
-    bool& keepParticle
+    const typename parcelType::trackingData& td
 )
 {
+    bool keepParticle = true;
+
     const auto& fzm = this->owner().mesh().faceZones();
 
     forAll(faceZoneIDs_, i)
@@ -322,8 +324,9 @@ void Foam::FaceInteraction<CloudType>::postFace
             }
         }
     }
+
+    return keepParticle;
 }
 
 
 // ************************************************************************* //
-

src/lagrangian/intermediate/submodels/CloudFunctionObjects/FaceInteraction/FaceInteraction.H
@@ -182,7 +182,11 @@ public:
     // Member Functions
 
         //- Post-face hook
-        virtual void postFace(const parcelType& p, bool& keepParticle);
+        virtual bool postFace
+        (
+            const parcelType& p,
+            const typename parcelType::trackingData& td
+        );
 };
 
 

src/lagrangian/intermediate/submodels/CloudFunctionObjects/FacePostProcessing/FacePostProcessing.C
@@ -350,7 +350,11 @@ Foam::FacePostProcessing<CloudType>::FacePostProcessing
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class CloudType>
-void Foam::FacePostProcessing<CloudType>::postFace(const parcelType& p, bool&)
+bool Foam::FacePostProcessing<CloudType>::postFace
+(
+    const parcelType& p,
+    const typename parcelType::trackingData& td
+)
 {
     if
     (
@@ -380,6 +384,8 @@ void Foam::FacePostProcessing<CloudType>::postFace(const parcelType& p, bool&)
             }
         }
     }
+
+    return true;
 }
 
 

src/lagrangian/intermediate/submodels/CloudFunctionObjects/FacePostProcessing/FacePostProcessing.H
@@ -158,7 +158,11 @@ public:
         inline bool resetOnWrite() const;
 
         //- Post-face hook
-        virtual void postFace(const parcelType& p, bool& keepParticle);
+        virtual bool postFace
+        (
+            const parcelType& p,
+            const typename parcelType::trackingData& td
+        );
 };
 
 

commit 7d9bba67c14bbecca4018fd392ebad19aa416387

BUG: Fixed keepParticle regression introduced in 8fb148bb

src/lagrangian/intermediate/submodels/CloudFunctionObjects/CloudFunctionObjectList/CloudFunctionObjectList.C
@@ -139,6 +139,11 @@ bool Foam::CloudFunctionObjectList<CloudType>::postMove
     const typename parcelType::trackingData& td
 )
 {
+    if (!td.keepParticle)
+    {
+        return td.keepParticle;
+    }
+
     for (auto& cfo : *this)
     {
         if (!cfo.postMove(p, dt, position0, td))
@@ -159,6 +164,11 @@ bool Foam::CloudFunctionObjectList<CloudType>::postPatch
     const typename parcelType::trackingData& td
 )
 {
+    if (!td.keepParticle)
+    {
+        return td.keepParticle;
+    }
+
     for (auto& cfo : *this)
     {
         if (!cfo.postPatch(p, pp, td))
@@ -178,13 +188,17 @@ bool Foam::CloudFunctionObjectList<CloudType>::postFace
     const typename parcelType::trackingData& td
 )
 {
+    if (!td.keepParticle)
+    {
+        return td.keepParticle;
+    }
+
     for (auto& cfo : *this)
     {
         if (!cfo.postFace(p, td))
         {
             return false;
         }
-
     }
 
     return true;