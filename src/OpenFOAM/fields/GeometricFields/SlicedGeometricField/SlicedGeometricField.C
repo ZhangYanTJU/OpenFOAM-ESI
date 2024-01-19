ROCm/OpenFOAM_HMM

commit e98acdc4fc140f93bb07911ff853d8ca2fd0a4f7

ENH: extend type aliases to include geometric boundary fields (#2348)
STYLE: LduInterfaceFieldPtrsList as alias instead of a class

STYLE: define patch lists typedefs when defining the base patch

- eg, polyPatchList typedef within polyPatch.H

INT: relocate GeometricField::Boundary -> GeometricBoundaryField

- was internal to GeometricField but moving it outside simplifies
  forward declarations etc. Code adapted from openfoam.org

src/OpenFOAM/fields/GeometricFields/SlicedGeometricField/SlicedGeometricField.C
@@ -6,6 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2022 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -47,11 +48,8 @@ slicedBoundaryField
     const bool preserveProcessorOnly
 )
 {
-    tmp<FieldField<PatchField, Type>> tbf
-    (
-        new FieldField<PatchField, Type>(mesh.boundary().size())
-    );
-    FieldField<PatchField, Type>& bf = tbf.ref();
+    auto tbf = tmp<FieldField<PatchField, Type>>::New(mesh.boundary().size());
+    auto& bf = tbf.ref();
 
     forAll(mesh.boundary(), patchi)
     {
@@ -123,11 +121,8 @@ slicedBoundaryField
     const bool preserveCouples
 )
 {
-    tmp<FieldField<PatchField, Type>> tbf
-    (
-        new FieldField<PatchField, Type>(mesh.boundary().size())
-    );
-    FieldField<PatchField, Type>& bf = tbf.ref();
+    auto tbf = tmp<FieldField<PatchField, Type>>::New(mesh.boundary().size());
+    auto& bf = tbf.ref();
 
     forAll(mesh.boundary(), patchi)
     {
@@ -157,10 +152,10 @@ slicedBoundaryField
                 new SlicedPatchField<Type>
                 (
                     mesh.boundary()[patchi],
-                    DimensionedField<Type, GeoMesh>::null()
+                    DimensionedField<Type, GeoMesh>::null(),
+                    bField[patchi]
                 )
             );
-            bf[patchi].UList<Type>::shallowCopy(bField[patchi]);
         }
     }
 
@@ -169,38 +164,6 @@ slicedBoundaryField
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-
-template
-<
-    class Type,
-    template<class> class PatchField,
-    template<class> class SlicedPatchField,
-    class GeoMesh
->
-Foam::SlicedGeometricField<Type, PatchField, SlicedPatchField, GeoMesh>::
-Internal::Internal
-(
-    const IOobject& io,
-    const Mesh& mesh,
-    const dimensionSet& ds,
-    const Field<Type>& iField
-)
-:
-    DimensionedField<Type, GeoMesh>
-    (
-        io,
-        mesh,
-        ds,
-        Field<Type>()
-    )
-{
-    // Set the internalField to the slice of the complete field
-    UList<Type>::shallowCopy
-    (
-        typename Field<Type>::subField(iField, GeoMesh::size(mesh))
-    );
-}
-
 
 template
 <
@@ -228,10 +191,10 @@ SlicedGeometricField
         slicedBoundaryField(mesh, completeField, preserveCouples)
     )
 {
-    // Set the internalField to the slice of the complete field
+    // Set internalField to the slice of the complete field
     UList<Type>::shallowCopy
     (
-        typename Field<Type>::subField(completeField, GeoMesh::size(mesh))
+        SubList<Type>(completeField, GeoMesh::size(mesh))
     );
 
     correctBoundaryConditions();
@@ -272,10 +235,10 @@ SlicedGeometricField
         )
     )
 {
-    // Set the internalField to the slice of the complete field
+    // Set internalField to the slice of the complete field
     UList<Type>::shallowCopy
     (
-        typename Field<Type>::subField(completeIField, GeoMesh::size(mesh))
+        SubList<Type>(completeIField, GeoMesh::size(mesh))
     );
 
     correctBoundaryConditions();
@@ -306,7 +269,7 @@ SlicedGeometricField
         slicedBoundaryField(gf.mesh(), gf.boundaryField(), preserveCouples)
     )
 {
-    // Set the internalField to the supplied internal field
+    // Set internalField to the internal field
     UList<Type>::shallowCopy(gf.primitiveField());
 
     correctBoundaryConditions();
@@ -335,7 +298,7 @@ SlicedGeometricField
         slicedBoundaryField(gf.mesh(), gf.boundaryField(), true)
     )
 {
-    // Set the internalField to the supplied internal field
+    // Set internalField to the internal field
     UList<Type>::shallowCopy(gf.primitiveField());
 }
 
@@ -357,12 +320,9 @@ clone() const
     return tmp
     <
         SlicedGeometricField<Type, PatchField, SlicedPatchField, GeoMesh>
-    >
+    >::New
     (
-        new SlicedGeometricField<Type, PatchField, SlicedPatchField, GeoMesh>
-        (
-            *this
-        )
+        *this
     );
 }
 
@@ -379,25 +339,8 @@ template
 Foam::SlicedGeometricField<Type, PatchField, SlicedPatchField, GeoMesh>::
 ~SlicedGeometricField()
 {
-    // Set the internalField storage pointer to nullptr before its destruction
-    // to protect the field it a slice of.
-    UList<Type>::shallowCopy(UList<Type>(nullptr, 0));
-}
-
-
-template
-<
-    class Type,
-    template<class> class PatchField,
-    template<class> class SlicedPatchField,
-    class GeoMesh
->
-Foam::SlicedGeometricField<Type, PatchField, SlicedPatchField, GeoMesh>::
-Internal::~Internal()
-{
-    // Set the internalField storage pointer to nullptr before its destruction
-    // to protect the field it a slice of.
-    UList<Type>::shallowCopy(UList<Type>(nullptr, 0));
+    // Set internalField to nullptr to avoid deletion of underlying field
+    UList<Type>::shallowCopy(UList<Type>());
 }
 
 

applications/utilities/parallelProcessing/decomposePar/domainDecomposition.C
@@ -419,7 +419,7 @@ bool Foam::domainDecomposition::writeDecomposition(const bool decomposeSets)
             nInterProcPatches += curSubPatchIDs[procPatchi].size();
         }
 
-        PtrList<polyPatch> procPatches
+        polyPatchList procPatches
         (
             curPatchSizes.size() + nInterProcPatches
         );

applications/utilities/parallelProcessing/redistributePar/parFvFieldReconstructorFields.C
@@ -129,8 +129,7 @@ Foam::parFvFieldReconstructor::reconstructFvVolumeField
 
     PtrList<fvPatchField<Type>> patchFields(fld.mesh().boundary().size());
 
-    const typename GeometricField<Type, fvPatchField, volMesh>::Boundary&
-        bfld = fld.boundaryField();
+    const auto& bfld = fld.boundaryField();
 
     forAll(bfld, patchI)
     {
@@ -293,8 +292,7 @@ Foam::parFvFieldReconstructor::reconstructFvSurfaceField
 
     PtrList<fvsPatchField<Type>> patchFields(fld.mesh().boundary().size());
 
-    const typename GeometricField<Type, fvsPatchField, surfaceMesh>::Boundary&
-        bfld = fld.boundaryField();
+    const auto& bfld = fld.boundaryField();
 
     forAll(bfld, patchI)
     {

src/OpenFOAM/fields/DimensionedFields/DimensionedField/SlicedDimensionedField.H
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2011 OpenFOAM Foundation
+    Copyright (C) 2022 OpenFOAM Foundation
     Copyright (C) 2022 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
@@ -24,64 +24,70 @@ License
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
-InClass
-    Foam::surfaceFields
+Class
+    Foam::SlicedDimensionedField
 
 Description
-
-SourceFiles
-    surfaceFields.C
+    Specialisation of DimensionedField that holds a slice of a given
+    field so that it acts as a DimensionedField.
 
 \*---------------------------------------------------------------------------*/
 
-#ifndef Foam_surfaceFieldsFwd_H
-#define Foam_surfaceFieldsFwd_H
+#ifndef Foam_SlicedDimensionedField_H
+#define Foam_SlicedDimensionedField_H
 
-#include "fieldTypes.H"
+#include "DimensionedField.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
 
-// Geometric field (internal + boundary)
-template<class Type, template<class> class PatchField, class GeoMesh>
-class GeometricField;
-
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-// Forward Declarations
-class surfaceMesh;
-template<class Type> class fvsPatchField;
-
-
-//- A (volume) surface field for a given type
-template<class Type>
-using SurfaceField = GeometricField<Type, fvsPatchField, surfaceMesh>;
-
-
-// Typedefs
-
-typedef GeometricField<scalar, fvsPatchField, surfaceMesh> surfaceScalarField;
-typedef GeometricField<vector, fvsPatchField, surfaceMesh> surfaceVectorField;
-typedef GeometricField<sphericalTensor, fvsPatchField, surfaceMesh>
-    surfaceSphericalTensorField;
-typedef GeometricField<symmTensor, fvsPatchField, surfaceMesh>
-    surfaceSymmTensorField;
-typedef GeometricField<tensor, fvsPatchField, surfaceMesh> surfaceTensorField;
-
-
 /*---------------------------------------------------------------------------*\
-                       Namespace fieldTypes Declaration
+                   Class SlicedDimensionedField Declaration
 \*---------------------------------------------------------------------------*/
 
-namespace fieldTypes
+template<class Type, class GeoMesh>
+class SlicedDimensionedField
+:
+    public DimensionedField<Type, GeoMesh>
 {
-    //- Standard surface field types (scalar, vector, tensor, etc)
-    extern const wordList surface;
+public:
 
-} // End namespace fieldTypes
+    // Public Typedefs
+
+        //- Type of mesh on which this SlicedDimensionedField is instantiated
+        typedef typename GeoMesh::Mesh Mesh;
+
+
+    // Constructors
+
+        //- Construct from components and field to slice
+        SlicedDimensionedField
+        (
+            const IOobject& io,
+            const Mesh& mesh,
+            const dimensionSet& ds,
+            const Field<Type>& iField
+        )
+        :
+            DimensionedField<Type, GeoMesh>(io, mesh, ds, Field<Type>())
+        {
+            // Set internalField to the slice of the complete field
+            UList<Type>::shallowCopy
+            (
+                SubList<Type>(iField, GeoMesh::size(mesh))
+            );
+        }
+
+
+    //- Destructor
+    ~SlicedDimensionedField()
+    {
+        // Set internalField to nullptr to avoid deletion of underlying field
+        UList<Type>::shallowCopy(UList<Type>());
+    }
+};
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //

src/OpenFOAM/fields/GeometricFields/GeometricField/GeometricBoundaryField.C
@@ -5,8 +5,8 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2011-2017 OpenFOAM Foundation
-    Copyright (C) 2016-2020 OpenCFD Ltd.
+    Copyright (C) 2011-2017,2022 OpenFOAM Foundation
+    Copyright (C) 2016-2022 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -26,23 +26,29 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "emptyPolyPatch.H"
+#include "GeometricBoundaryField.H"
 #include "globalMeshData.H"
 #include "cyclicPolyPatch.H"
+#include "emptyPolyPatch.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::readField
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::readField
 (
     const DimensionedField<Type, GeoMesh>& field,
     const dictionary& dict
 )
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     // Clear the boundary field if already initialised
     this->clear();
 
-    this->setSize(bmesh_.size());
+    this->resize(bmesh_.size());
 
     label nUnset = this->size();
 
@@ -180,7 +186,7 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::readField
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const BoundaryMesh& bmesh
 )
@@ -191,7 +197,7 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const BoundaryMesh& bmesh,
     const DimensionedField<Type, GeoMesh>& field,
@@ -201,7 +207,10 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
     FieldField<PatchField, Type>(bmesh.size()),
     bmesh_(bmesh)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     forAll(bmesh_, patchi)
     {
@@ -220,7 +229,7 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const BoundaryMesh& bmesh,
     const DimensionedField<Type, GeoMesh>& field,
@@ -231,7 +240,10 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
     FieldField<PatchField, Type>(bmesh.size()),
     bmesh_(bmesh)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     if
     (
@@ -284,7 +296,7 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const BoundaryMesh& bmesh,
     const DimensionedField<Type, GeoMesh>& field,
@@ -294,7 +306,10 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
     FieldField<PatchField, Type>(bmesh.size()),
     bmesh_(bmesh)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     forAll(bmesh_, patchi)
     {
@@ -304,16 +319,19 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const DimensionedField<Type, GeoMesh>& field,
-    const typename GeometricField<Type, PatchField, GeoMesh>::Boundary& btf
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& btf
 )
 :
     FieldField<PatchField, Type>(btf.size()),
     bmesh_(btf.bmesh_)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     forAll(bmesh_, patchi)
     {
@@ -323,10 +341,10 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const DimensionedField<Type, GeoMesh>& field,
-    const typename GeometricField<Type, PatchField, GeoMesh>::Boundary& btf,
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& btf,
     const labelList& patchIDs,
     const word& patchFieldType
 )
@@ -334,7 +352,10 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
     FieldField<PatchField, Type>(btf.size()),
     bmesh_(btf.bmesh_)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     for (const label patchi : patchIDs)
     {
@@ -361,20 +382,23 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
-    const typename GeometricField<Type, PatchField, GeoMesh>::Boundary& btf
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& btf
 )
 :
     FieldField<PatchField, Type>(btf),
     bmesh_(btf.bmesh_)
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 }
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::GeometricBoundaryField
 (
     const BoundaryMesh& bmesh,
     const DimensionedField<Type, GeoMesh>& field,
@@ -391,9 +415,12 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::Boundary
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::updateCoeffs()
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::updateCoeffs()
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     forAll(*this, patchi)
     {
@@ -403,9 +430,12 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::updateCoeffs()
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::evaluate()
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::evaluate()
 {
-    DebugInFunction << nl;
+    ///if (GeometricField<Type, PatchField, GeoMesh::debug)
+    ///{
+    ///    InfoInFunction << nl;
+    ///}
 
     if
     (
@@ -468,7 +498,7 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::evaluate()
 
 template<class Type, template<class> class PatchField, class GeoMesh>
 Foam::wordList
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::types() const
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::types() const
 {
     const FieldField<PatchField, Type>& pff = *this;
 
@@ -484,32 +514,30 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::types() const
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-typename Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::
 boundaryInternalField() const
 {
-    typename GeometricField<Type, PatchField, GeoMesh>::Boundary
-        BoundaryInternalField(*this);
+    GeometricBoundaryField<Type, PatchField, GeoMesh> result(*this);
 
-    forAll(BoundaryInternalField, patchi)
+    forAll(result, patchi)
     {
-        BoundaryInternalField[patchi] ==
-            this->operator[](patchi).patchInternalField();
+        result[patchi] == this->operator[](patchi).patchInternalField();
     }
 
-    return BoundaryInternalField;
+    return result;
 }
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
 Foam::LduInterfaceFieldPtrsList<Type>
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::interfaces() const
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::interfaces() const
 {
     LduInterfaceFieldPtrsList<Type> list(this->size());
 
     forAll(list, patchi)
     {
-        const LduInterfaceField<Type>* lduPtr =
+        const auto* lduPtr =
             isA<LduInterfaceField<Type>>(this->operator[](patchi));
 
         if (lduPtr)
@@ -524,14 +552,14 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::interfaces() const
 
 template<class Type, template<class> class PatchField, class GeoMesh>
 Foam::lduInterfaceFieldPtrsList
-Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::
+Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::
 scalarInterfaces() const
 {
     lduInterfaceFieldPtrsList list(this->size());
 
     forAll(list, patchi)
     {
-        const lduInterfaceField* lduPtr =
+        const auto* lduPtr =
             isA<lduInterfaceField>(this->operator[](patchi));
 
         if (lduPtr)
@@ -545,7 +573,7 @@ scalarInterfaces() const
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::writeEntry
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::writeEntry
 (
     const word& keyword,
     Ostream& os
@@ -560,7 +588,7 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::writeEntry
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::writeEntries
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::writeEntries
 (
     Ostream& os
 ) const
@@ -577,10 +605,9 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::writeEntries
 // * * * * * * * * * * * * * * * Member Operators  * * * * * * * * * * * * * //
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator=
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator=
 (
-    const typename GeometricField<Type, PatchField, GeoMesh>::
-    Boundary& bf
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& bf
 )
 {
     FieldField<PatchField, Type>::operator=(bf);
@@ -588,30 +615,29 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator=
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator=
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator=
 (
-    const FieldField<PatchField, Type>& ptff
+    const FieldField<PatchField, Type>& bf
 )
 {
-    FieldField<PatchField, Type>::operator=(ptff);
+    FieldField<PatchField, Type>::operator=(bf);
 }
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator=
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator=
 (
-    const Type& t
+    const Type& val
 )
 {
-    FieldField<PatchField, Type>::operator=(t);
+    FieldField<PatchField, Type>::operator=(val);
 }
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator==
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator==
 (
-    const typename GeometricField<Type, PatchField, GeoMesh>::
-    Boundary& bf
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& bf
 )
 {
     forAll(*this, patchi)
@@ -622,27 +648,27 @@ void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator==
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator==
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator==
 (
-    const FieldField<PatchField, Type>& ptff
+    const FieldField<PatchField, Type>& bf
 )
 {
     forAll(*this, patchi)
     {
-        this->operator[](patchi) == ptff[patchi];
+        this->operator[](patchi) == bf[patchi];
     }
 }
 
 
 template<class Type, template<class> class PatchField, class GeoMesh>
-void Foam::GeometricField<Type, PatchField, GeoMesh>::Boundary::operator==
+void Foam::GeometricBoundaryField<Type, PatchField, GeoMesh>::operator==
 (
-    const Type& t
+    const Type& val
 )
 {
     forAll(*this, patchi)
     {
-        this->operator[](patchi) == t;
+        this->operator[](patchi) == val;
     }
 }
 
@@ -653,8 +679,7 @@ template<class Type, template<class> class PatchField, class GeoMesh>
 Foam::Ostream& Foam::operator<<
 (
     Ostream& os,
-    const typename GeometricField<Type, PatchField, GeoMesh>::
-    Boundary& bf
+    const GeometricBoundaryField<Type, PatchField, GeoMesh>& bf
 )
 {
     os << static_cast<const FieldField<PatchField, Type>&>(bf);

ZhangYanTJU/OpenFOAM-ESI

commit ebe49d4cbd2f87dc4c5b0381c253e6540000383d

BUG: SlicedGeometricField, slices into field instead of shallow copy (#3080)
- regression introduced by e98acdc4fc14

  Affected versions: (v2206, v2212, v2306, v2312)

src/OpenFOAM/fields/GeometricFields/SlicedGeometricField/SlicedGeometricField.C
@@ -176,10 +176,11 @@ makeBoundary
                 new SlicedPatchField<Type>
                 (
                     p,
-                    DimensionedField<Type, GeoMesh>::null(),
-                    bField[patchi]
+                    DimensionedField<Type, GeoMesh>::null()
                 )
             );
+
+            bf[patchi].UList<Type>::shallowCopy(bField[patchi]);
         }
     }
 