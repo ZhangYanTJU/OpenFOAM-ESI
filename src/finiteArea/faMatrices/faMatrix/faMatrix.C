a12b70c1ceef483f7b5cc55373e6ebc19b30b30f
Kutalmis Bercin <kutalmis.bercin@esi-group.com>
2024-02-27 13:40:36 +0000

    ENH: fvMatrix, faMatrix:  replace raw pointers with unique_ptr (#3107)

--- src/finiteArea/faMatrices/faMatrix/faMatrix.C
+++ src/finiteArea/faMatrices/faMatrix/faMatrix.C
@@ -190,8 +190,7 @@ Foam::faMatrix<Type>::faMatrix
     dimensions_(ds),
     source_(psi.size(), Zero),
     internalCoeffs_(psi.mesh().boundary().size()),
-    boundaryCoeffs_(psi.mesh().boundary().size()),
-    faceFluxCorrectionPtr_(nullptr)
+    boundaryCoeffs_(psi.mesh().boundary().size())
 {
     DebugInFunction
         << "constructing faMatrix<Type> for field " << psi_.name()
@@ -231,19 +230,17 @@ Foam::faMatrix<Type>::faMatrix(const faMatrix<Type>& fam)
     dimensions_(fam.dimensions_),
     source_(fam.source_),
     internalCoeffs_(fam.internalCoeffs_),
-    boundaryCoeffs_(fam.boundaryCoeffs_),
-    faceFluxCorrectionPtr_(nullptr)
+    boundaryCoeffs_(fam.boundaryCoeffs_)
 {
     DebugInFunction
         << "Copying faMatrix<Type> for field " << psi_.name() << endl;
 
     if (fam.faceFluxCorrectionPtr_)
     {
-        faceFluxCorrectionPtr_ =
-            new GeometricField<Type, faePatchField, edgeMesh>
-            (
-                *(fam.faceFluxCorrectionPtr_)
-            );
+        faceFluxCorrectionPtr_ = std::make_unique<faceFluxFieldType>
+        (
+            *(fam.faceFluxCorrectionPtr_)
+        );
     }
 }
 
@@ -256,8 +253,7 @@ Foam::faMatrix<Type>::faMatrix(const tmp<faMatrix<Type>>& tmat)
     dimensions_(tmat().dimensions_),
     source_(tmat.constCast().source_, tmat.movable()),
     internalCoeffs_(tmat.constCast().internalCoeffs_, tmat.movable()),
-    boundaryCoeffs_(tmat.constCast().boundaryCoeffs_, tmat.movable()),
-    faceFluxCorrectionPtr_(nullptr)
+    boundaryCoeffs_(tmat.constCast().boundaryCoeffs_, tmat.movable())
 {
     DebugInFunction
         << "Copy/Move faMatrix<Type> for field " << psi_.name() << endl;
@@ -266,16 +262,15 @@ Foam::faMatrix<Type>::faMatrix(const tmp<faMatrix<Type>>& tmat)
     {
         if (tmat.movable())
         {
-            faceFluxCorrectionPtr_ = tmat().faceFluxCorrectionPtr_;
-            tmat().faceFluxCorrectionPtr_ = nullptr;
-        }
-        else
-        {
             faceFluxCorrectionPtr_ =
-                new GeometricField<Type, faePatchField, edgeMesh>
-                (
-                    *(tmat().faceFluxCorrectionPtr_)
-                );
+                std::move(tmat.constCast().faceFluxCorrectionPtr_);
+        }
+        else if (tmat().faceFluxCorrectionPtr_)
+        {
+            faceFluxCorrectionPtr_ = std::make_unique<faceFluxFieldType>
+            (
+                *(tmat().faceFluxCorrectionPtr_)
+            );
         }
     }
 
@@ -290,8 +285,6 @@ Foam::faMatrix<Type>::~faMatrix()
 {
     DebugInFunction
         << "Destroying faMatrix<Type> for field " << psi_.name() << endl;
-
-    deleteDemandDrivenData(faceFluxCorrectionPtr_);
 }
 
 
@@ -788,9 +781,10 @@ void Foam::faMatrix<Type>::operator=(const faMatrix<Type>& famv)
     }
     else if (famv.faceFluxCorrectionPtr_)
     {
-        faceFluxCorrectionPtr_ =
-            new GeometricField<Type, faePatchField, edgeMesh>
-            (*famv.faceFluxCorrectionPtr_);
+        faceFluxCorrectionPtr_ = std::make_unique<faceFluxFieldType>
+        (
+            *famv.faceFluxCorrectionPtr_
+        );
     }
 }
 
@@ -835,8 +829,7 @@ void Foam::faMatrix<Type>::operator+=(const faMatrix<Type>& famv)
     }
     else if (famv.faceFluxCorrectionPtr_)
     {
-        faceFluxCorrectionPtr_ = new
-        GeometricField<Type, faePatchField, edgeMesh>
+        faceFluxCorrectionPtr_ = std::make_unique<faceFluxFieldType>
         (
             *famv.faceFluxCorrectionPtr_
         );
@@ -869,9 +862,10 @@ void Foam::faMatrix<Type>::operator-=(const faMatrix<Type>& famv)
     }
     else if (famv.faceFluxCorrectionPtr_)
     {
-        faceFluxCorrectionPtr_ =
-            new GeometricField<Type, faePatchField, edgeMesh>
-            (-*famv.faceFluxCorrectionPtr_);
+        faceFluxCorrectionPtr_ = std::make_unique<faceFluxFieldType>
+        (
+            -*famv.faceFluxCorrectionPtr_
+        );
     }
 }
 
