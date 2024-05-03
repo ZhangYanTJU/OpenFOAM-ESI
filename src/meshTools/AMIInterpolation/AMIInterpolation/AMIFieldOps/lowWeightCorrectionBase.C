e44184e626dfd294182427cb754eba54aea5c9ce
Andrew Heather <>
2024-05-03 16:05:37 +0100

    ENH: AMI - added new set of field-based AMI operations

---
+++ src/meshTools/AMIInterpolation/AMIInterpolation/AMIFieldOps/lowWeightCorrectionBase.C
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2024 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "lowWeightCorrectionBase.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+const Foam::Enum<Foam::lowWeightCorrectionBase::option>
+Foam::lowWeightCorrectionBase::optionNames_
+({
+    { option::NONE, "none" },
+    { option::ASSIGN, "assign" },
+    { option::BLEND, "blend" },
+});
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::lowWeightCorrectionBase::lowWeightCorrectionBase(const option& opt)
+:
+    opt_(opt)
+{}
+
+
+// ************************************************************************* //
